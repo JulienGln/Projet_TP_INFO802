456c48340dce19f873b77661700676acc26fed30
JulienGln <juliengalerne.jg@gmail.com>
2024-01-11 17:16:35 +0100

    ajout api carte leaflet

--- electro_trajet/src/components/Map.jsx
+++ electro_trajet/src/components/Map.jsx
@@ -1,22 +1,32 @@
-import React from "react";
-import Openrouteservice from "openrouteservice-js";
+import React, { useEffect, useRef } from "react";
+// import Openrouteservice from "openrouteservice-js";
+import * as L from "leaflet";
 
 export default function Map() {
-  const orsDirections = new Openrouteservice.Directions({
-    api_key: "Votre clé API",
-  });
+  // const orsDirections = new Openrouteservice.Directions({
+  //   api_key: "Votre clé API",
+  // });
 
-  return (
-    <div>
-      <iframe
-        width="600"
-        height="450"
-        loading="lazy"
-        allowfullscreen
-        referrerpolicy="no-referrer-when-downgrade"
-        src="https://www.google.com/maps/embed/v1/place?key=
-    &q=Space+Needle,Seattle+WA"
-      ></iframe>
-    </div>
-  );
+  const mapRef = useRef(null); // éviter de s'afficher plusieurs fois
+
+  useEffect(() => {
+    if (!mapRef.current) {
+      mapRef.current = L.map("map", { zoom: 13 }).setView(
+        [48.8566, 2.3522],
+        13
+      );
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "© OpenStreetMap contributors",
+      }).addTo(mapRef.current);
+    }
+
+    // Ajoutez un écouteur d'événements pour le zoom
+    mapRef.current.on("zoomend", function () {
+      // Ajustez la taille de la carte après le zoom
+      mapRef.current.invalidateSize();
+    });
+  }, []);
+
+  return <div id="map" style={{ height: "500px", width: "50%" }}></div>;
 }
